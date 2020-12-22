f5b01036fc23612f1d3ed51fca304eccdb3687f5
pieeri <eric@rubicubix.co.uk>
2020-12-22 20:07:27 +0100

    Add auth mixin for basic or token client auth

---
+++ src/tests/test_clients/test_auth.py
@@ -0,0 +1,43 @@
+import pytest
+import requests
+
+from noos_pyk.clients import auth, json
+
+
+@pytest.fixture
+def auth_class():
+    class TokenAuth(auth.HTTPTokenAuth):
+        default_header = "Test-Header"
+        default_value = "testKey"
+
+    return TokenAuth
+
+
+@pytest.fixture
+def auth_client(auth_class):
+    class AuthClient(json.JSONClient, auth.AuthClient):
+        default_auth_class = auth_class
+
+    return AuthClient
+
+
+@pytest.fixture
+def mocked_request(mocker, auth_client):
+    mocker.patch.object(auth_client, "_check")
+    mocker.patch.object(auth_client, "_deserialize")
+    return mocker.patch.object(requests.Session, "send")
+
+
+def test_bearer_token_auth_header_correctly_formed(auth_client, mocked_request):
+    token = "test_token"
+    client = auth_client(base_url="http://hostname/")
+    client.set_auth_header(token)
+
+    client.get("v1/test-endpoint")
+
+    mocked_request.assert_called_once()
+
+    args, _ = mocked_request.call_args
+
+    assert "Test-Header" in args[0].headers
+    assert args[0].headers["Test-Header"] == "testKey test_token"
